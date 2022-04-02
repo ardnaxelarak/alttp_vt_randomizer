4196230845a50b5285d730189d120feab25722f0
Kara Alexandra <ardnaxelarak@gmail.com>
2022-04-01 18:44:23 -0700

    Update base rom to fix disaster

--- app/Rom.php
+++ app/Rom.php
@@ -14,7 +14,7 @@ class Rom
 {
     const BUILD_INFO = [
         'base' => ['BUILD' => '2022-01-24', 'HASH' => 'afb7608cc456b1c6be0efe9ef223121b'],
-        'overworld' => ['BUILD' => '2022-03-31', 'HASH' => '7dfe74a0f5f6d895206ddcaeec4f1a83'],
+        'overworld' => ['BUILD' => '2022-04-01', 'HASH' => '12e7127dab898b4f706062c0c8c60034'],
     ];
     const SIZE = 2097152;
 
